48d09022a1959b47162354e0517abf31e32bf405
Dan Wagner <daniel.wagner@linkshare.com>
2014-04-21 09:41:27 -0500

    initial rails app built with generate

---
+++ flix/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Flix::Application.config.secret_key_base = '5ada9654cbac93503a46c22fdd177acda54021f3731e94eb65cc5f574f4eb340a50baf4142999536563f251be33b7f53d02cf6c67dd6ef78a6ecda8e37a20590'
