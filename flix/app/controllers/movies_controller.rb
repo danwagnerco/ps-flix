7d12aa6c37d8e70f7e73ef42b993c1155d78491d
Dan Wagner <daniel.wagner@linkshare.com>
2014-04-28 11:49:24 -0500

    work through lesson 7, helpers

--- flix/app/controllers/movies_controller.rb
+++ flix/app/controllers/movies_controller.rb
@@ -2,5 +2,6 @@ class MoviesController < ApplicationController
   def index
     # @movies = ['Iron Man', 'Superman', 'Spider-Man', 'Batman']
     @movies = Movie.all
+    # @movies = Movie.last(1)
   end
 end
